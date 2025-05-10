15e6c44a1ebb6f477f16660a14530abbb915c3e2
Emilio-Magana <shockwave0024@gmail.com>
2025-05-10 21:21:39 +0700

    env fix

--- src/config/vectordb.ts
+++ src/config/vectordb.ts
@@ -1,6 +1,6 @@
 // // WHen using vercel: only use dotenv in dev as it calls for a local path
-// import { config } from "dotenv";
-// config({ path: "../.env" }); // Load .env into process.env
+import { config } from "dotenv";
+config({ path: ".env" }); // Load .env into process.env
 
 import { DataAPIClient } from "@datastax/astra-db-ts";
 import { AstraDBVectorStore } from "@langchain/community/vectorstores/astradb";
@@ -25,7 +25,7 @@ export async function getVectorStore() {
       collectionOptions: {
         vector: { dimension: 1536, metric: "cosine" },
       },
-    }
+    },
   );
 }
 
