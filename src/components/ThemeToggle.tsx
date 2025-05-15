28e2f0dacfc40d0f0c6ee37d1cd38d3098e24d12
Emilio-Magana <shockwave0024@gmail.com>
2025-05-15 13:32:10 +0700

    fixing themebutton

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,17 +1,32 @@
 "use client";
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { GiBarbedSun, GiEvilMoon } from "react-icons/gi";
 // import { useDarkMode } from "../../archive/context/DarkModeContext";
 
 export default function ThemeToggle() {
   // const { isDarkMode, toggleDarkMode } = useDarkMode();
-  const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   function handleTheme() {
-    setTheme(theme === "dark" ? "light" : "dark");
+    setTheme(resolvedTheme === "dark" ? "light" : "dark");
   }
   return (
     <button className="hover:text-hover" onClick={handleTheme}>
-      {theme === "light" ? <GiBarbedSun size={20} /> : <GiEvilMoon size={20} />}
+      {resolvedTheme === "dark" ? (
+        <GiBarbedSun size={20} />
+      ) : (
+        <GiEvilMoon size={20} />
+      )}
     </button>
   );
 }
